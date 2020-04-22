16cdff5bf00f0492f1a088ac0bbb412a1a00a91f
Grigory <ovonely@gmail.com>
2020-04-22 20:30:19 +0700

    копирование App.java из se-02

--- src/main/java/ru/naumkin/tm/App.java
+++ src/main/java/ru/naumkin/tm/App.java
@@ -1,13 +1,28 @@
 package ru.naumkin.tm;
 
-/**
- * Hello world!
- *
- */
+import ru.naumkin.tm.util.CommandHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "*** Welcome to task manager ***" );
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            CommandHandler commandHandler = new CommandHandler(reader);
+
+            while (true) {
+                String command = reader.readLine().toLowerCase();
+                if (command.equals("exit")) {
+                    System.exit(1);
+                } else {
+                    commandHandler.handleCommand(command);
+                }
+
+            }
+
+        }
     }
 }
