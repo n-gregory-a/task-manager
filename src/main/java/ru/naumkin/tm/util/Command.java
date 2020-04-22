bfc32b8ec91ca8350f45c5401ffda57c9ff1b51e
Grigory <ovonely@gmail.com>
2020-04-22 20:52:39 +0700

    создание перечисления Command

---
+++ src/main/java/ru/naumkin/tm/util/Command.java
@@ -0,0 +1,18 @@
+package ru.naumkin.tm.util;
+
+public enum Command {
+
+    HELP,
+    PROJECT_CLEAR,
+    PROJECT_CREATE,
+    PROJECT_LIST,
+    PROJECT_NAME,
+    PROJECT_UPDATE,
+    PROJECT_REMOVE,
+    TASK_CLEAR,
+    TASK_CREATE,
+    TASK_LIST,
+    TASK_REMOVE,
+    TASK_UPDATE
+
+}
