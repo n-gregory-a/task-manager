c5b5cfa2b9363397b044dd1f45c056cc4444ca95
Grigory <ovonely@gmail.com>
2020-04-23 14:17:47 +0700

    Project:  - добавление геттеров-сеттеров для полей

--- src/main/java/ru/naumkin/tm/entity/Project.java
+++ src/main/java/ru/naumkin/tm/entity/Project.java
@@ -1,6 +1,7 @@
 package ru.naumkin.tm.entity;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class Project {
@@ -24,6 +25,44 @@ public class Project {
         this.name = name;
         this.description = description;
         this.startDate = LocalDate.parse(startDate);
+        this.endDate = LocalDate.parse(endDate);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return formatter.format(startDate);
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = LocalDate.parse(startDate);
+    }
+
+    public String getEndDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return formatter.format(endDate);
+    }
+
+    public void setEndDate(String endDate) {
         this.endDate = LocalDate.parse(endDate);
     }
 }
