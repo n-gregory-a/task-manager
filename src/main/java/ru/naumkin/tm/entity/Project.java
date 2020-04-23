94c1943887f9ee8a2c5693186a6016fc8431bea7
Grigory <ovonely@gmail.com>
2020-04-23 21:55:35 +0700

    Project: добавление поля для хранения списка задач в метод toString()

--- src/main/java/ru/naumkin/tm/entity/Project.java
+++ src/main/java/ru/naumkin/tm/entity/Project.java
@@ -67,6 +67,7 @@ public class Project {
                 ", description='" + description + '\'' +
                 ", dateStart=" + dateStart +
                 ", dateFinish=" + dateFinish +
+                ", taskIdList=" + taskIdList +
                 '}';
     }
 }
