65fc453c79a3dec7e3ac1e34b3523a2e97191116
Grigory <ovonely@gmail.com>
2020-04-27 19:13:33 +0700

    рефакторинг класса Application

--- src/main/java/ru/naumkin/tm/Application.java
+++ src/main/java/ru/naumkin/tm/Application.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.init();
     }
