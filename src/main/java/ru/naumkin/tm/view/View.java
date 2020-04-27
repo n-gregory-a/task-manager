5478ef81977c39885b50fc85369ad8960b0d6c5d
Grigory <ovonely@gmail.com>
2020-04-27 19:10:43 +0700

    рефакторинг класса View

--- src/main/java/ru/naumkin/tm/view/View.java
+++ src/main/java/ru/naumkin/tm/view/View.java
@@ -11,26 +11,6 @@ public class View {
         this.reader = reader;
     }
 
-    public void showHelp() {
-        System.out.println(
-                "help: Show all commands." +
-                        "\nproject-clear: Remove all projects." +
-                        "\nproject-create: Create new project." +
-                        "\nproject-update: Update project" +
-                        "\nproject-list: Show all projects." +
-                        "\nproject-name: Show project by name." +
-                        "\nproject-remove: Remove selected project" +
-                        "\ntask-clear: Remove all tasks." +
-                        "\ntask-create: Create new task." +
-                        "\ntask-update: Update task" +
-                        "\ntask-list: Show all tasks." +
-                        "\ntask-name: Show task by name" +
-                        "\ntask-attach: attach task to the project" +
-                        "\ntasks-view: show all tasks attached to the project" +
-                        "\nexit: Exit program."
-        );
-    }
-
     public void showMessage(String message) {
         System.out.println(message);
     }
