2f726ce53a7970c812bfbc4dcfa79bf73c2b822c
Grigory <ovonely@gmail.com>
2020-04-29 20:19:07 +0700

    изменение класса команды UserUpdateCommand

--- src/main/java/ru/naumkin/tm/command/UserUpdateCommand.java
+++ src/main/java/ru/naumkin/tm/command/UserUpdateCommand.java
@@ -1,5 +1,7 @@
 package ru.naumkin.tm.command;
 
+import ru.naumkin.tm.entity.User;
+
 public class UserUpdateCommand extends AbstractCommand {
 
     @Override
@@ -14,7 +16,19 @@ public class UserUpdateCommand extends AbstractCommand {
 
     @Override
     public void execute() throws Exception {
-
+        bootstrap.getView().showMessage("[USER UPDATE PROFILE]");
+        User user = bootstrap.getCurrentUser();
+        String login = user.getLogin();
+        bootstrap.getView().showMessage("Enter new login:");
+        String newLogin = bootstrap.getView().readLine();
+        user.setLogin(newLogin);
+        try {
+            bootstrap.getUserService().merge(user, login);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            bootstrap.getView().showMessage(e.getMessage());
+            return;
+        }
+        bootstrap.getView().showMessage("[OK]");
     }
 
 }
