21b4fc3daffb5abcda17d22f0fade21c32b2c4c5
Grigory <ovonely@gmail.com>
2020-04-29 15:03:34 +0700

    изменение класса команды UserAuthoriseCommand

--- src/main/java/ru/naumkin/tm/command/UserAuthoriseCommand.java
+++ src/main/java/ru/naumkin/tm/command/UserAuthoriseCommand.java
@@ -1,5 +1,10 @@
 package ru.naumkin.tm.command;
 
+import ru.naumkin.tm.entity.User;
+import ru.naumkin.tm.util.HashGenerator;
+
+import java.io.IOException;
+
 public class UserAuthoriseCommand extends AbstractCommand {
 
     @Override
@@ -14,7 +19,28 @@ public class UserAuthoriseCommand extends AbstractCommand {
 
     @Override
     public void execute() throws Exception {
+        bootstrap.getView().showMessage("[USER AUTHORISATION]");
+        User user = getUserByName();
+        bootstrap.getView().showMessage("Enter password");
+        String password = bootstrap.getView().readLine();
+        boolean passwordIsCorrect = HashGenerator.getHash(password).equals(user.getPassword());
+        if (!passwordIsCorrect) {
+            bootstrap.getView().showMessage("Password is incorrect. Authorisation failed.");
+            return;
+        }
+        bootstrap.setCurrentUser(user);
+    }
 
+    private User getUserByName() throws IOException {
+        bootstrap.getView().showMessage("Enter login");
+        User user;
+        try {
+            user = bootstrap.getUserService().findOne(bootstrap.getView().readLine());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            bootstrap.getView().showMessage(e.getMessage());
+            user = getUserByName();
+        }
+        return user;
     }
 
 }
