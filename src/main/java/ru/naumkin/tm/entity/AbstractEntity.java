88e72c1519bc446b131a0db074b00d74fceaf5ad
Grigory <ovonely@gmail.com>
2020-05-02 18:06:33 +0700

    Создание класса AbstractEntity.

---
+++ src/main/java/ru/naumkin/tm/entity/AbstractEntity.java
@@ -0,0 +1,16 @@
+package ru.naumkin.tm.entity;
+
+import java.util.UUID;
+
+public abstract class AbstractEntity {
+
+    private String id = UUID.randomUUID().toString();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
