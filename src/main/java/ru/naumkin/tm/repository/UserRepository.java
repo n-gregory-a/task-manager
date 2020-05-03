89b8dda4a716663dd7363d3cf9f82c43cda5fb7d
Grigory <ovonely@gmail.com>
2020-05-03 19:12:37 +0700

    Рефакторинг UserRepository.

--- src/main/java/ru/naumkin/tm/repository/UserRepository.java
+++ src/main/java/ru/naumkin/tm/repository/UserRepository.java
@@ -2,42 +2,15 @@ package ru.naumkin.tm.repository;
 
 import ru.naumkin.tm.entity.User;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
+public class UserRepository extends AbstractRepository<User> {
 
-public class UserRepository {
-
-    private final Map<String, User> users = new LinkedHashMap<>();
-
-    public Collection<User> findAll() {
-        return users.values();
-    }
-
-    public User findOne(String login) {
-        return users.get(login);
-    }
-
-    public User persist(User user) {
-        return users.put(user.getLogin(), user);
-    }
-
-    public User merge(User user, String login) throws NoSuchAlgorithmException {
-        User updatingUser = users.get(login);
-        updatingUser.setLogin(login);
+    @Override
+    public User merge(User user, String name) {
+        User updatingUser = map.get(name);
+        updatingUser.setName(name);
         updatingUser.setPassword(user.getPassword());
         updatingUser.setRole(user.getRole());
-        users.remove(login);
-        return users.put(updatingUser.getLogin(), updatingUser);
+        map.remove(name);
+        return map.put(updatingUser.getName(), updatingUser);
     }
-
-    public User remove(User user) {
-        return users.remove(user.getLogin());
-    }
-
-    public void removeAll() {
-        users.clear();
-    }
-
 }
