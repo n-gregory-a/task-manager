ae75f0a0d50db264879edc6b753d8a0e2842a58a
Grigory <ovonely@gmail.com>
2020-05-04 19:00:05 +0700

    Рефакторинг UserService.

--- src/main/java/ru/naumkin/tm/service/UserService.java
+++ src/main/java/ru/naumkin/tm/service/UserService.java
@@ -1,77 +1,12 @@
 package ru.naumkin.tm.service;
 
+import ru.naumkin.tm.api.repository.IRepository;
 import ru.naumkin.tm.entity.User;
-import ru.naumkin.tm.error.NameIsEmptyException;
-import ru.naumkin.tm.error.NameIsNullException;
-import ru.naumkin.tm.error.NoUserWithSuchLoginException;
-import ru.naumkin.tm.error.UserIsNullException;
-import ru.naumkin.tm.repository.UserRepository;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.Collection;
+public class UserService extends AbstractService<User> {
 
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public Collection<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    public User findOne(String login) {
-        if (login == null) {
-            throw new NameIsNullException();
-        }
-        if (login.isEmpty()) {
-            throw new NameIsEmptyException();
-        }
-        User user = userRepository.findOne(login);
-        if (user == null) {
-            throw new NoUserWithSuchLoginException(login);
-        }
-        return user;
-    }
-
-    public User persist(User user) {
-        if (user == null) {
-            throw new UserIsNullException();
-        }
-        return userRepository.persist(user);
-    }
-
-    public User merge(User user, String login) throws NoSuchAlgorithmException {
-        if (login == null) {
-            throw new NameIsNullException();
-        }
-        if (login.isEmpty()) {
-            throw new NameIsEmptyException();
-        }
-        if (user == null) {
-            throw new UserIsNullException();
-        }
-        if (user.getName().isEmpty()) {
-            throw new NameIsEmptyException();
-        }
-        User updatingUser = userRepository.findOne(login);
-        if (updatingUser == null) {
-            return userRepository.persist(user);
-        }
-        return userRepository.merge(user);
-    }
-
-    public User remove(User user) {
-        if (user == null) {
-            throw new UserIsNullException();
-        }
-        return userRepository.remove(user);
-    }
-
-    public void removeAll() {
-        userRepository.removeAll();
+    public UserService(IRepository<User> repository) {
+        super(repository);
     }
 
 }
