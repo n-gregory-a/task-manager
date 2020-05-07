a8ea52562a16d430921a1ed2b5a02b4002df016f
Grigory <ovonely@gmail.com>
2020-05-07 22:28:59 +0700

    Добавление аннотаций @NotNull и @Nullable в исключениях.

--- src/main/java/ru/naumkin/tm/error/NoProjectWithSuchNameException.java
+++ src/main/java/ru/naumkin/tm/error/NoProjectWithSuchNameException.java
@@ -1,10 +1,12 @@
 package ru.naumkin.tm.error;
 
+import org.jetbrains.annotations.NotNull;
+
 public final class NoProjectWithSuchNameException extends RuntimeException {
 
     private final String name;
 
-    public NoProjectWithSuchNameException(final String name) {
+    public NoProjectWithSuchNameException(final @NotNull String name) {
         this.name = name;
     }
 
--- src/main/java/ru/naumkin/tm/error/NoTaskWithSuchNameException.java
+++ src/main/java/ru/naumkin/tm/error/NoTaskWithSuchNameException.java
@@ -1,10 +1,12 @@
 package ru.naumkin.tm.error;
 
+import org.jetbrains.annotations.NotNull;
+
 public final class NoTaskWithSuchNameException extends RuntimeException {
 
     private final String name;
 
-    public NoTaskWithSuchNameException(final String name) {
+    public NoTaskWithSuchNameException(final @NotNull String name) {
         this.name = name;
     }
 
