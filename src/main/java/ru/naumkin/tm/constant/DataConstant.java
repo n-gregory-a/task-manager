cb1037bf07ff8797abd73d271462f87e37190dde
Grigory <ovonely@gmail.com>
2020-05-12 20:56:57 +0700

    Создание класса DataConstant.

---
+++ src/main/java/ru/naumkin/tm/constant/DataConstant.java
@@ -0,0 +1,11 @@
+package ru.naumkin.tm.constant;
+
+public class DataConstant {
+
+    public static final String BINARY_FILE = "./data.bin";
+
+    public static final String XML_FILE = "./data.xml";
+
+    public static final String JSON_FILE = "./data.json";
+
+}
