a3cffb9f582bd885aed3d9ed79b16c65e90ecbbf
Grigory <ovonely@gmail.com>
2020-05-12 22:01:41 +0700

    Применение к AbstractEntity интерфейса Serializable.

--- src/main/java/ru/naumkin/tm/entity/AbstractEntity.java
+++ src/main/java/ru/naumkin/tm/entity/AbstractEntity.java
@@ -3,12 +3,13 @@ package ru.naumkin.tm.entity;
 import lombok.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public abstract class AbstractEntity {
+public abstract class AbstractEntity implements Serializable {
 
     @NotNull
     private String id = UUID.randomUUID().toString();
