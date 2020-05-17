ceea73fa9747da2ab52f280607fc0bf531a53d62
Grigory <ovonely@gmail.com>
2020-05-17 21:28:28 +0700

    Руфакторинг IDomainEndpoint.

--- tm-client/src/main/java/ru/naumkin/tm/api/endpoint/IDomainEndpoint.java
+++ tm-client/src/main/java/ru/naumkin/tm/api/endpoint/IDomainEndpoint.java
@@ -21,39 +21,39 @@ import javax.xml.ws.ResponseWrapper;
 public interface IDomainEndpoint {
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataFasterXmlResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataFasterXml/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataFasterXmlResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataFasterXml/Fault/Exception")})
     @RequestWrapper(localName = "loadJsonDataFasterXml", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadJsonDataFasterXml")
     @ResponseWrapper(localName = "loadJsonDataFasterXmlResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadJsonDataFasterXmlResponse")
     public void loadJsonDataFasterXml()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveBinaryDataRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveBinaryDataResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveBinaryData/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveBinaryDataRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveBinaryDataResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveBinaryData/Fault/Exception")})
     @RequestWrapper(localName = "saveBinaryData", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveBinaryData")
     @ResponseWrapper(localName = "saveBinaryDataResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveBinaryDataResponse")
     public void saveBinaryData()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataFasterXmlResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataFasterXml/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataFasterXmlResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataFasterXml/Fault/Exception")})
     @RequestWrapper(localName = "saveJsonDataFasterXml", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveJsonDataFasterXml")
     @ResponseWrapper(localName = "saveJsonDataFasterXmlResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveJsonDataFasterXmlResponse")
     public void saveJsonDataFasterXml()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataJaxbResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataJaxb/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataJaxbResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveJsonDataJaxb/Fault/Exception")})
     @RequestWrapper(localName = "saveJsonDataJaxb", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveJsonDataJaxb")
     @ResponseWrapper(localName = "saveJsonDataJaxbResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveJsonDataJaxbResponse")
     public void saveJsonDataJaxb()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataFasterXmlResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataFasterXml/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataFasterXmlResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataFasterXml/Fault/Exception")})
     @RequestWrapper(localName = "loadXmlDataFasterXml", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadXmlDataFasterXml")
     @ResponseWrapper(localName = "loadXmlDataFasterXmlResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadXmlDataFasterXmlResponse")
     public void loadXmlDataFasterXml()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
     @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadResponse")
@@ -64,18 +64,18 @@ public interface IDomainEndpoint {
 ;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataJaxbResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataJaxb/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataJaxbResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadJsonDataJaxb/Fault/Exception")})
     @RequestWrapper(localName = "loadJsonDataJaxb", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadJsonDataJaxb")
     @ResponseWrapper(localName = "loadJsonDataJaxbResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadJsonDataJaxbResponse")
     public void loadJsonDataJaxb()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataJaxbResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataJaxb/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataJaxbResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataJaxb/Fault/Exception")})
     @RequestWrapper(localName = "saveXmlDataJaxb", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveXmlDataJaxb")
     @ResponseWrapper(localName = "saveXmlDataJaxbResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveXmlDataJaxbResponse")
     public void saveXmlDataJaxb()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
     @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveResponse")
@@ -88,23 +88,23 @@ public interface IDomainEndpoint {
     );
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataFasterXmlResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataFasterXml/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataFasterXmlRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataFasterXmlResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/saveXmlDataFasterXml/Fault/Exception")})
     @RequestWrapper(localName = "saveXmlDataFasterXml", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveXmlDataFasterXml")
     @ResponseWrapper(localName = "saveXmlDataFasterXmlResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.SaveXmlDataFasterXmlResponse")
     public void saveXmlDataFasterXml()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataJaxbResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataJaxb/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataJaxbRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataJaxbResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadXmlDataJaxb/Fault/Exception")})
     @RequestWrapper(localName = "loadXmlDataJaxb", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadXmlDataJaxb")
     @ResponseWrapper(localName = "loadXmlDataJaxbResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadXmlDataJaxbResponse")
     public void loadXmlDataJaxb()
- throws Exception_Exception;
+ throws Exception;
 
     @WebMethod
-    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadBinaryDataRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadBinaryDataResponse", fault = {@FaultAction(className = Exception_Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadBinaryData/Fault/Exception")})
+    @Action(input = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadBinaryDataRequest", output = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadBinaryDataResponse", fault = {@FaultAction(className = Exception.class, value = "http://endpoint.api.tm.naumkin.ru/IDomainEndpoint/loadBinaryData/Fault/Exception")})
     @RequestWrapper(localName = "loadBinaryData", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadBinaryData")
     @ResponseWrapper(localName = "loadBinaryDataResponse", targetNamespace = "http://endpoint.api.tm.naumkin.ru/", className = "ru.naumkin.tm.api.endpoint.LoadBinaryDataResponse")
     public void loadBinaryData()
- throws Exception_Exception;
+ throws Exception;
 }
