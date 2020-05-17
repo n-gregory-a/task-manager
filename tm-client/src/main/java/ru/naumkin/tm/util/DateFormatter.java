b1cee942501a61a7fe71776b8cdf253fd518da07
Grigory <ovonely@gmail.com>
2020-05-17 17:25:12 +0700

    Добвление в класс DateFormatter методов для коныертации XMLGregorianCalendar.

--- tm-client/src/main/java/ru/naumkin/tm/util/DateFormatter.java
+++ tm-client/src/main/java/ru/naumkin/tm/util/DateFormatter.java
@@ -3,15 +3,20 @@ package ru.naumkin.tm.util;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public final class DateFormatter {
 
     @NotNull
     private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("dd.MM.yyyy");
 
+    @NotNull
     public static String convertDateToString(@Nullable final Date date) {
         if (date == null) {
             return "not assigned";
@@ -19,8 +24,23 @@ public final class DateFormatter {
         return FORMATTER.format(date);
     }
 
+    @NotNull
     public static Date convertStringToDate(@NotNull final String date) throws ParseException {
         return FORMATTER.parse(date);
+    }
+
+    @NotNull
+    public static XMLGregorianCalendar convertToXmlGregorianCalendar(
+            @NotNull final Date date
+    ) throws DatatypeConfigurationException {
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        return DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+    }
+
+    @NotNull
+    public static Date convertToDate(@NotNull final XMLGregorianCalendar calendar) {
+        return calendar.toGregorianCalendar().getTime();
     }
 
 }
