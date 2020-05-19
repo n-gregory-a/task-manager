4e14b679ff41ea85a59d2b95b5cb87054fc07252
Grigory <ovonely@gmail.com>
2020-05-19 18:43:26 +0700

    Генерация клиента по wsdl.

--- tm-client/src/main/java/ru/naumkin/tm/api/endpoint/CreateUser.java
+++ tm-client/src/main/java/ru/naumkin/tm/api/endpoint/CreateUser.java
@@ -17,8 +17,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent&gt;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
  *       &lt;sequence&gt;
- *         &lt;element name="arg0" type="{http://endpoint.api.tm.naumkin.ru/}session" minOccurs="0"/&gt;
- *         &lt;element name="arg1" type="{http://endpoint.api.tm.naumkin.ru/}roleType" minOccurs="0"/&gt;
+ *         &lt;element name="arg0" type="{http://endpoint.api.tm.naumkin.ru/}roleType" minOccurs="0"/&gt;
  *       &lt;/sequence&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
@@ -29,24 +28,22 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "createUser", propOrder = {
-    "arg0",
-    "arg1"
+    "arg0"
 })
 public class CreateUser {
 
-    protected Session arg0;
     @XmlSchemaType(name = "string")
-    protected RoleType arg1;
+    protected RoleType arg0;
 
     /**
      * Gets the value of the arg0 property.
      * 
      * @return
      *     possible object is
-     *     {@link Session }
+     *     {@link RoleType }
      *     
      */
-    public Session getArg0() {
+    public RoleType getArg0() {
         return arg0;
     }
 
@@ -55,35 +52,11 @@ public class CreateUser {
      * 
      * @param value
      *     allowed object is
-     *     {@link Session }
+     *     {@link RoleType }
      *     
      */
-    public void setArg0(Session value) {
+    public void setArg0(RoleType value) {
         this.arg0 = value;
-    }
-
-    /**
-     * Gets the value of the arg1 property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link RoleType }
-     *     
-     */
-    public RoleType getArg1() {
-        return arg1;
-    }
-
-    /**
-     * Sets the value of the arg1 property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link RoleType }
-     *     
-     */
-    public void setArg1(RoleType value) {
-        this.arg1 = value;
     }
 
 }
