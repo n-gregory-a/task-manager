ceea9ef617a0a22e6c230f7111155fbefdff5bb5
Grigory <ovonely@gmail.com>
2020-05-19 13:22:27 +0700

    Рефакторинг ProjectEndpoint.

--- tm-server/src/main/java/ru/naumkin/tm/api/endpoint/IProjectEndpoint.java
+++ tm-server/src/main/java/ru/naumkin/tm/api/endpoint/IProjectEndpoint.java
@@ -3,6 +3,7 @@ package ru.naumkin.tm.api.endpoint;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.naumkin.tm.entity.Project;
+import ru.naumkin.tm.entity.Session;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
@@ -13,63 +14,82 @@ public interface IProjectEndpoint {
 
     @NotNull
     @WebMethod
-    List<Project> findAllProjects();
+    List<Project> findAllProjects(@Nullable final Session session);
 
     @NotNull
     @WebMethod
-    Project findOneProject(@Nullable final String name);
+    Project findOneProject(@Nullable final Session session,
+                           @Nullable final String name);
 
     @Nullable
     @WebMethod
-    Project persistProject(@Nullable final Project project);
+    Project persistProject(@Nullable final Session session,
+                           @Nullable final Project project);
 
     @Nullable
     @WebMethod
-    Project mergeProject(@Nullable final Project project, @Nullable final String name);
+    Project mergeProject(@Nullable final Session session,
+                         @Nullable final Project project,
+                         @Nullable final String name);
 
     @Nullable
     @WebMethod
-    Project removeProject(@Nullable final Project project);
+    Project removeProject(@Nullable final Session session,
+                          @Nullable final Project project);
 
     @WebMethod
-    void removeAllProjects();
+    void removeAllProjects(@Nullable final Session session);
 
     @WebMethod
-    void loadProject(@NotNull final Project[] projects);
-
-    @NotNull
-    @WebMethod
-    List<Project> findAllProjectsByUserId(@Nullable final String currentUserId);
+    void loadProject(@Nullable final Session session,
+                     @NotNull final Project[] projects);
 
     @NotNull
     @WebMethod
-    Project findOneProjectByUserId(@Nullable final String currentUserId, @Nullable final String name);
+    List<Project> findAllProjectsByUserId(@Nullable final Session session,
+                                          @Nullable final String currentUserId);
 
     @NotNull
     @WebMethod
-    Project removeProjectByUserId(@NotNull final String currentUserId, @NotNull final Project project);
-
-    @WebMethod
-    void removeAllProjectsByUserId(@Nullable final String currentUserId);
-
-    @NotNull
-    @WebMethod
-    List<Project> sortProjectsByDateStart(@Nullable final String currentUserId);
+    Project findOneProjectByUserId(@Nullable final Session session,
+                                   @Nullable final String currentUserId,
+                                   @Nullable final String name);
 
     @NotNull
     @WebMethod
-    List<Project> sortProjectsByDateFinish(@Nullable final String currentUserId);
+    Project removeProjectByUserId(@Nullable final Session session,
+                                  @NotNull final String currentUserId,
+                                  @NotNull final Project project);
+
+    @WebMethod
+    void removeAllProjectsByUserId(@Nullable final Session session,
+                                   @Nullable final String currentUserId);
 
     @NotNull
     @WebMethod
-    List<Project> sortProjectsByStatus(@NotNull final String currentUserId);
+    List<Project> sortProjectsByDateStart(@Nullable final Session session,
+                                          @Nullable final String currentUserId);
 
     @NotNull
     @WebMethod
-    List<Project> sortProjectsByName(@NotNull final String currentUserId, @NotNull final String name);
+    List<Project> sortProjectsByDateFinish(@Nullable final Session session,
+                                           @Nullable final String currentUserId);
 
     @NotNull
     @WebMethod
-    List<Project> sortProjectsByDescription(@NotNull final String currentUserId, @NotNull final String description);
+    List<Project> sortProjectsByStatus(@Nullable final Session session,
+                                       @NotNull final String currentUserId);
+
+    @NotNull
+    @WebMethod
+    List<Project> sortProjectsByName(@Nullable final Session session,
+                                     @NotNull final String currentUserId,
+                                     @NotNull final String name);
+
+    @NotNull
+    @WebMethod
+    List<Project> sortProjectsByDescription(@Nullable final Session session,
+                                            @NotNull final String currentUserId,
+                                            @NotNull final String description);
 
 }
--- tm-server/src/main/java/ru/naumkin/tm/enpoint/ProjectEndpoint.java
+++ tm-server/src/main/java/ru/naumkin/tm/enpoint/ProjectEndpoint.java
@@ -5,7 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.naumkin.tm.api.endpoint.IProjectEndpoint;
 import ru.naumkin.tm.api.service.IProjectService;
+import ru.naumkin.tm.api.service.ISessionService;
 import ru.naumkin.tm.entity.Project;
+import ru.naumkin.tm.entity.Session;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
@@ -14,32 +16,41 @@ import java.util.List;
 
 @NoArgsConstructor
 @WebService(endpointInterface = "ru.naumkin.tm.api.endpoint.IProjectEndpoint")
-public final class ProjectEndpoint implements IProjectEndpoint {
+public final class ProjectEndpoint extends AbstractEndpoint implements IProjectEndpoint {
 
     @NotNull private IProjectService projectService;
 
-    public ProjectEndpoint(@NotNull final IProjectService projectService) {
+    public ProjectEndpoint(
+            @NotNull final ISessionService sessionService,
+            @NotNull final IProjectService projectService
+    ) {
+        super(sessionService);
         this.projectService = projectService;
     }
 
     @NotNull
     @Override
     @WebMethod
-    public List<Project> findAllProjects() {
+    public List<Project> findAllProjects(@Nullable final Session session) {
+        validate(session);
         return new LinkedList<>(projectService.findAll());
     }
 
     @NotNull
     @Override
     @WebMethod
-    public Project findOneProject(@Nullable final String name) {
+    public Project findOneProject(@Nullable final Session session,
+                                  @Nullable final String name) {
+        validate(session);
         return projectService.findOne(name);
     }
 
     @Nullable
     @Override
     @WebMethod
-    public Project persistProject(@Nullable final Project project) {
+    public Project persistProject(@Nullable final Session session,
+                                  @Nullable final Project project) {
+        validate(session);
         return projectService.persist(project);
     }
 
@@ -47,35 +58,47 @@ public final class ProjectEndpoint implements IProjectEndpoint {
     @Override
     @WebMethod
     public Project mergeProject(
+            @Nullable final Session session,
             @Nullable final Project project,
             @Nullable final String name
     ) {
+        validate(session);
         return projectService.merge(project, name);
     }
 
     @Nullable
     @Override
     @WebMethod
-    public Project removeProject(@Nullable final Project project) {
+    public Project removeProject(@Nullable final Session session,
+                                 @Nullable final Project project
+    ) {
+        validate(session);
         return projectService.remove(project);
     }
 
     @Override
     @WebMethod
-    public void removeAllProjects() {
+    public void removeAllProjects(@Nullable final Session session) {
+        validate(session);
         projectService.removeAll();
     }
 
     @Override
     @WebMethod
-    public void loadProject(@NotNull final Project[] projects) {
+    public void loadProject(@Nullable final Session session,
+                            @NotNull final Project[] projects) {
+        validate(session);
         projectService.persist(projects);
     }
 
     @NotNull
     @Override
     @WebMethod
-    public List<Project> findAllProjectsByUserId(@Nullable final String currentUserId) {
+    public List<Project> findAllProjectsByUserId(
+            @Nullable final Session session,
+            @Nullable final String currentUserId
+    ) {
+        validate(session);
         return projectService.findAll(currentUserId);
     }
 
@@ -83,9 +106,11 @@ public final class ProjectEndpoint implements IProjectEndpoint {
     @Override
     @WebMethod
     public Project findOneProjectByUserId(
+            @Nullable final Session session,
             @Nullable final String currentUserId,
             @Nullable final String name
     ) {
+        validate(session);
         return projectService.findOne(currentUserId, name);
     }
 
@@ -93,36 +118,54 @@ public final class ProjectEndpoint implements IProjectEndpoint {
     @Override
     @WebMethod
     public Project removeProjectByUserId(
+            @Nullable final Session session,
             @NotNull final String currentUserId,
             @NotNull final Project project
     ) {
+        validate(session);
         return projectService.remove(currentUserId, project);
     }
 
     @Override
     @WebMethod
-    public void removeAllProjectsByUserId(@Nullable final String currentUserId) {
+    public void removeAllProjectsByUserId(
+            @Nullable final Session session,
+            @Nullable final String currentUserId
+    ) {
+        validate(session);
         projectService.findAll(currentUserId);
     }
 
     @NotNull
     @Override
     @WebMethod
-    public List<Project> sortProjectsByDateStart(@Nullable final String currentUserId) {
+    public List<Project> sortProjectsByDateStart(
+            @Nullable final Session session,
+            @Nullable final String currentUserId
+    ) {
+        validate(session);
         return projectService.sortByDateStart(currentUserId);
     }
 
     @NotNull
     @Override
     @WebMethod
-    public List<Project> sortProjectsByDateFinish(@Nullable final String currentUserId) {
+    public List<Project> sortProjectsByDateFinish(
+            @Nullable final Session session,
+            @Nullable final String currentUserId
+    ) {
+        validate(session);
         return projectService.sortByDateFinish(currentUserId);
     }
 
     @NotNull
     @Override
     @WebMethod
-    public List<Project> sortProjectsByStatus(@NotNull final String currentUserId) {
+    public List<Project> sortProjectsByStatus(
+            @Nullable final Session session,
+            @NotNull final String currentUserId
+    ) {
+        validate(session);
         return projectService.sortByStatus(currentUserId);
     }
 
@@ -130,9 +173,11 @@ public final class ProjectEndpoint implements IProjectEndpoint {
     @Override
     @WebMethod
     public List<Project> sortProjectsByName(
+            @Nullable final Session session,
             @NotNull final String currentUserId,
             @NotNull final String name
     ) {
+        validate(session);
         return projectService.sortByName(currentUserId, name);
     }
 
@@ -140,9 +185,11 @@ public final class ProjectEndpoint implements IProjectEndpoint {
     @Override
     @WebMethod
     public List<Project> sortProjectsByDescription(
+            @Nullable final Session session,
             @NotNull final String currentUserId,
             @NotNull final String description
     ) {
+        validate(session);
         return projectService.sortByDescription(currentUserId, description);
     }
 
