13e290ff86593781a0f9572ba2fde63eb71950b6
Grigory <ovonely@gmail.com>
2020-05-21 17:55:10 +0700

    Добавление FieldConstant.

---
+++ tm-server/src/main/java/ru/naumkin/tm/constant/FieldConstant.java
@@ -0,0 +1,20 @@
+package ru.naumkin.tm.constant;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FieldConstant {
+
+    @NotNull public static final String ID = "id";
+    @NotNull public static final String NAME = "name";
+    @NotNull public static final String DESCRIPTION = "description";
+    @NotNull public static final String DATE_START = "date_start";
+    @NotNull public static final String DATE_FINISH = "date_finish";
+    @NotNull public static final String USER_ID = "user-id";
+    @NotNull public static final String PROJECT_ID = "project-id";
+    @NotNull public static final String STATUS = "status";
+    @NotNull public static final String PASSWORD_HASH = "password_hash";
+    @NotNull public static final String ROLE = "role";
+    @NotNull public static final String TIMESTAMP = "timestamp";
+    @NotNull public static final String SIGNATURE = "signature";
+
+}
