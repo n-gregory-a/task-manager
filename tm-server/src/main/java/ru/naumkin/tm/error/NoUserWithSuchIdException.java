114c9aeb498725218025ab7993c0519fd5473563
Grigory <ovonely@gmail.com>
2020-05-22 13:49:45 +0700

    Создание NoUserWithSuchIdException.

---
+++ tm-server/src/main/java/ru/naumkin/tm/error/NoUserWithSuchIdException.java
@@ -0,0 +1,8 @@
+package ru.naumkin.tm.error;
+
+public class NoUserWithSuchIdException extends RuntimeException {
+
+    public NoUserWithSuchIdException(String s) {
+        super("No user with id " + s);
+    }
+}
