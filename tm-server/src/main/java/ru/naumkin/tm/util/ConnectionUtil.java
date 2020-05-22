200725304d91539eea7181a7fb42db51729c4e85
Grigory <ovonely@gmail.com>
2020-05-22 23:20:04 +0700

    Изменение настроек подключения к БД.

--- tm-server/src/main/java/ru/naumkin/tm/util/ConnectionUtil.java
+++ tm-server/src/main/java/ru/naumkin/tm/util/ConnectionUtil.java
@@ -3,6 +3,7 @@ package ru.naumkin.tm.util;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -10,10 +11,12 @@ public final class ConnectionUtil {
 
     @NotNull
     public static Connection getConnection(
+            @NotNull final String driver,
             @NotNull final String url,
             @NotNull final String userName,
             @NotNull final String password
-    ) throws SQLException {
+    ) throws SQLException, ClassNotFoundException {
+        Class.forName(driver);
         return DriverManager.getConnection(url, userName, password);
     }
 
