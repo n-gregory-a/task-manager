eeb1cd2cde171d26bff7658d30aa04822636ae30
Grigory <ovonely@gmail.com>
2020-05-23 16:29:57 +0700

    Рефакторинг FieldConstant.

--- tm-server/src/main/java/ru/naumkin/tm/constant/FieldConstant.java
+++ tm-server/src/main/java/ru/naumkin/tm/constant/FieldConstant.java
@@ -9,8 +9,8 @@ public class FieldConstant {
     @NotNull public static final String DESCRIPTION = "description";
     @NotNull public static final String DATE_START = "date_start";
     @NotNull public static final String DATE_FINISH = "date_finish";
-    @NotNull public static final String USER_ID = "user-id";
-    @NotNull public static final String PROJECT_ID = "project-id";
+    @NotNull public static final String USER_ID = "user_id";
+    @NotNull public static final String PROJECT_ID = "project_id";
     @NotNull public static final String STATUS = "status";
     @NotNull public static final String PASSWORD_HASH = "password_hash";
     @NotNull public static final String ROLE = "role";
