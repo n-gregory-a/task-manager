e172d546abb68b0e5f514dcef4dc68579ca28b7a
Grigory <ovonely@gmail.com>
2020-05-25 21:45:27 +0700

    Рефакторинг ITaskRepository.

--- tm-server/src/main/java/ru/naumkin/tm/api/repository/ITaskRepository.java
+++ tm-server/src/main/java/ru/naumkin/tm/api/repository/ITaskRepository.java
@@ -1,5 +1,9 @@
 package ru.naumkin.tm.api.repository;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.naumkin.tm.entity.Task;
@@ -10,38 +14,71 @@ import java.util.List;
 public interface ITaskRepository {
 
     @NotNull
-    List<Task> findAll() throws SQLException;
+    @Select("SELECT * FROM `task`")
+    List<Task> findAll() throws Exception;
 
     @NotNull
-    List<Task> findAll(@NotNull final String currentUserId) throws SQLException;
+    @Select("SELECT * FROM `task` " +
+            "WHERE `user_id` = #{userId}")
+    List<Task> findAll(@NotNull final String userId) throws Exception;
 
     @Nullable
-    Task findOne(@NotNull final String currentUserId, @NotNull final String name) throws SQLException;
+    @Select("SELECT * FROM `task` " +
+            "WHERE `name` = #{name} " +
+            "AND `user_id` = #{userId}")
+    Task findOneByUserId(@NotNull final String userId, @NotNull final String name) throws Exception;
 
     @Nullable
-    Task persist(@NotNull final Task task) throws SQLException;
+    @Insert("INSERT INTO `task` " +
+            "(`id`, `name`, `description`, `date_start`, `date_finish`, `project_id`, `user_id`, `status`) " +
+            "VALUES (#{id}, #{name}, #{description}, #{dateStart}, #{dateFinish}, #{projectId}, #{userId}, #{status})")
+    Task persist(@NotNull final Task task) throws Exception;
 
     @Nullable
-    Task merge(@NotNull final Task task) throws SQLException;
+    @Update("UPDATE `task` " +
+            "SET `name` = #{name}, `description` = #{description}, `date_start` = #{dateStart}," +
+            "`date_finish` = #{dateFinish}, `project_id` = #{projectId}, `user_id` = #{userId}, `status` = #{status}" +
+            "WHERE `id` = #{id}")
+    Task merge(@NotNull final Task task) throws Exception;
 
     @Nullable
-    Task remove(@NotNull final String currentUserId, @NotNull final Task task) throws SQLException;
+    @Delete("DELETE FROM `task` " +
+            "WHERE `id` = #{id} " +
+            "AND `user_id` = #{userId}")
+    Task remove(@NotNull final String userId, @NotNull final Task task) throws Exception;
 
-    void removeAll(@NotNull final String currentUserId) throws SQLException;
-
-    @NotNull
-    List<Task> sortByDateStart(@NotNull final String currentUserId) throws SQLException;
-
-    @NotNull
-    List<Task> sortByDateFinish(@NotNull final String currentUserId) throws SQLException;
-
-    @NotNull
-    List<Task> sortByStatus(@NotNull final String currentUserId) throws SQLException;
+    @Delete("DELETE FROM `task` " +
+            "WHERE `user_id` = #{userId}")
+    void removeAll(@NotNull final String userId) throws Exception;
 
     @NotNull
-    List<Task> sortByName(@NotNull final String currentUserId, @NotNull final String name) throws SQLException;
+    @Select("SELECT * FROM `task` " +
+            "WHERE `user_id` = #{userId} " +
+            "ORDER BY `date_start`")
+    List<Task> sortByDateStart(@NotNull final String userId) throws Exception;
 
     @NotNull
-    List<Task> sortByDescription(@NotNull final String currentUserId, @NotNull final String description) throws SQLException;
+    @Select("SELECT * FROM `task` " +
+            "WHERE `user_id` = #{userId} " +
+            "ORDER BY `date_finish`")
+    List<Task> sortByDateFinish(@NotNull final String userId) throws Exception;
+
+    @NotNull
+    @Select("SELECT * FROM `task` " +
+            "WHERE `user_id` = #{userId} " +
+            "ORDER `status`")
+    List<Task> sortByStatus(@NotNull final String userId) throws Exception;
+
+    @NotNull
+    @Select("SELECT * FROM `task` " +
+            "WHERE `user_id` = #{userId} " +
+            "AND `name` LIKE `%#{name}%`")
+    List<Task> sortByName(@NotNull final String userId, @NotNull final String name) throws Exception;
+
+    @NotNull
+    @Select("SELECT * FROM `task` " +
+            "WHERE `user_id` = #{userId} " +
+            "AND `description` LIKE `%#{description}%`")
+    List<Task> sortByDescription(@NotNull final String userId, @NotNull final String description) throws Exception;
 
 }
