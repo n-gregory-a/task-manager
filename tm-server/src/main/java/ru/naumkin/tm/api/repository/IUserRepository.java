72709fdcf6effb36fe897516341f7f965c7482d4
Grigory <ovonely@gmail.com>
2020-05-25 21:54:28 +0700

    Рефакторинг IUserRepository.

--- tm-server/src/main/java/ru/naumkin/tm/api/repository/IUserRepository.java
+++ tm-server/src/main/java/ru/naumkin/tm/api/repository/IUserRepository.java
@@ -1,32 +1,50 @@
 package ru.naumkin.tm.api.repository;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.naumkin.tm.entity.User;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public interface IUserRepository {
 
     @NotNull
-    List<User> findAll() throws SQLException;
+    @Select("SELECT * FROM `app_user`")
+    List<User> findAll() throws Exception;
 
     @Nullable
-    User findOne(@NotNull final String name) throws SQLException;
+    @Select("SELECT * FROM `app_user` " +
+            "WHERE `name` = #{name}")
+    User findOne(@NotNull final String name) throws Exception;
 
     @Nullable
-    User findOneById(@NotNull final String id) throws SQLException;
+    @Select("SELECT * FROM `app_user` " +
+            "WHERE `id` = #{id}")
+    User findOneById(@NotNull final String id) throws Exception;
 
     @Nullable
-    User persist(@NotNull final User user) throws SQLException;
+    @Insert("INSERT INTO `app_user` " +
+            "(`id`, `name`, `password_hash`, `role`) " +
+            "VALUES (#{id}, #{name}, #{password}, #{role})")
+    User persist(@NotNull final User user) throws Exception;
 
     @Nullable
-    User merge(@NotNull final User user) throws SQLException;
+    @Update("UPDATE `app_user` " +
+            "SET `name` = #{name}, " +
+            "`password_hash` = #{password}, `role` = #{role}" +
+            "WHERE `id` = #{id}")
+    User merge(@NotNull final User user) throws Exception;
 
     @Nullable
-    User remove(@NotNull final User user) throws SQLException;
+    @Delete("DELETE FROM `app_user` " +
+            "WHERE `id` = #{id}")
+    User remove(@NotNull final User user) throws Exception;
 
-    void removeAll() throws SQLException;
+    @Delete("DELETE * FROM `app_user`")
+    void removeAll() throws Exception;
 
 }
