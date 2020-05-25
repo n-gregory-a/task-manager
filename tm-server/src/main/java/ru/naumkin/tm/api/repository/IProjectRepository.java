321ac4601e4ed8637e75455b9ca3d14397de6c0c
Grigory <ovonely@gmail.com>
2020-05-25 21:15:28 +0700

    Рефакторинг IProjectRepository.

--- tm-server/src/main/java/ru/naumkin/tm/api/repository/IProjectRepository.java
+++ tm-server/src/main/java/ru/naumkin/tm/api/repository/IProjectRepository.java
@@ -30,53 +30,53 @@ public interface IProjectRepository {
     @Nullable
     @Insert("INSERT INTO `project` " +
             "(`id`, `name`, `description`, `date_start`, `date_finish`, `user_id`, `status`) " +
-            "VALUES (#{id}, #{name}, #{description}, #{date_start}, #{date_finish}, #{user_id}, #{status})")
+            "VALUES (#{id}, #{name}, #{description}, #{dateStart}, #{dateFinish}, #{userId}, #{status})")
     Project persist(@NotNull final Project project) throws Exception;
 
     @Nullable
     @Update("UPDATE `project` " +
-            "SET `name` = #{name}, `description` = #{description}, `date_start` = #{date_start}," +
-            "`date_finish` = #{date_finish}, `user_id` = #{user_id}, `status` = #{status}" +
+            "SET `name` = #{name}, `description` = #{description}, `date_start` = #{dateStart}," +
+            "`date_finish` = #{dateFinish}, `user_id` = #{userId}, `status` = #{status}" +
             "WHERE `id` = #{id}")
     Project merge(@NotNull final Project project) throws Exception;
 
     @Nullable
     @Delete("DELETE FROM `project` " +
             "WHERE `id` = #{id} " +
-            "AND `user_id` = #{user_id}")
+            "AND `user_id` = #{userId}")
     Project remove(@NotNull final String userId, @NotNull final Project project) throws Exception;
 
     @Delete("DELETE FROM `project` " +
-            "WHERE `user_id` = #{user_id}")
+            "WHERE `user_id` = #{userId}")
     void removeAll(@NotNull final String userId) throws Exception;
 
     @NotNull
     @Select("SELECT * FROM `project` " +
-            "WHERE `user_id` = #{user_id} " +
+            "WHERE `user_id` = #{userId} " +
             "ORDER BY `date_start`")
     List<Project> sortByDateStart(@NotNull final String userId) throws Exception;
 
     @NotNull
     @Select("SELECT * FROM `project` " +
-            "WHERE `user_id` = #{user_id} " +
+            "WHERE `user_id` = #{userId} " +
             "ORDER BY `date_finish`")
     List<Project> sortByDateFinish(@NotNull final String userId) throws Exception;
 
     @NotNull
     @Select("SELECT * FROM `project` " +
-            "WHERE `user_id` = #{user_id} " +
+            "WHERE `user_id` = #{userId} " +
             "ORDER BY `status`")
     List<Project> sortByStatus(@NotNull final String userId) throws Exception;
 
     @NotNull
     @Select("SELECT * FROM `project` " +
-            "WHERE `user_id` = #{user_id} " +
+            "WHERE `user_id` = #{userId} " +
             "AND `name` LIKE `%#{name}%`")
     List<Project> sortByName(@NotNull final String userId, @NotNull final String name) throws Exception;
 
     @NotNull
     @Select("SELECT * FROM `project` " +
-            "WHERE `user_id` = #{user_id} " +
+            "WHERE `user_id` = #{userId} " +
             "AND `description` LIKE `%#{description}%`")
     List<Project> sortByDescription(@NotNull final String userId, @NotNull final String description) throws Exception;
 
