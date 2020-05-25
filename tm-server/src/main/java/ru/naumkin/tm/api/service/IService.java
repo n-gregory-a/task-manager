000547067cbc1465d842893f484d92151529e07f
Grigory <ovonely@gmail.com>
2020-05-25 22:02:35 +0700

    Рефакторинг IService.

--- tm-server/src/main/java/ru/naumkin/tm/api/service/IService.java
+++ tm-server/src/main/java/ru/naumkin/tm/api/service/IService.java
@@ -1,14 +1,12 @@
 package ru.naumkin.tm.api.service;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.jetbrains.annotations.NotNull;
 import ru.naumkin.tm.entity.AbstractEntity;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public interface IService<E extends AbstractEntity> {
 
     @NotNull
-    Connection getConnection() throws SQLException, ClassNotFoundException;
+    SqlSessionFactory getSqlSessionFactory();
 
 }
