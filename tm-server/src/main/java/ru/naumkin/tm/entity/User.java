044f7050ca051cfe3e808a78208e2bf1ee2740fc
Grigory <ovonely@gmail.com>
2020-05-30 18:34:57 +0700

    Расстановка аннотаций Hibernate, создание графов сущностей Project, Task, Session в классе User.

--- tm-server/src/main/java/ru/naumkin/tm/entity/User.java
+++ tm-server/src/main/java/ru/naumkin/tm/entity/User.java
@@ -7,22 +7,35 @@ import org.jetbrains.annotations.NotNull;
 import ru.naumkin.tm.enumerated.RoleType;
 import ru.naumkin.tm.util.HashGenerator;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public final class User extends AbstractEntity {
 
     @NotNull
+    @Column(name = "password_hash")
     private String password = HashGenerator.getHash("password");
 
     @NotNull
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
     private RoleType role = RoleType.USER;
 
-    @Override
-    public String toString() {
-        return "login: " + getName() +
-                ", role: " + role.displayName() +
-                '}';
-    }
+    @NotNull
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Project> projects = new ArrayList<>();
+
+    @NotNull
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+
+    @NotNull
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Session> sessions = new ArrayList<>();
 
 }
