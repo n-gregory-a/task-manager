9e946b1f3f85e22eb557aa2c96b3b33b61cbc40f
Grigory <ovonely@gmail.com>
2020-05-30 18:05:56 +0700

    Расстановка анотаций Hibernate в классе AbstractEntity.

--- tm-server/src/main/java/ru/naumkin/tm/entity/AbstractEntity.java
+++ tm-server/src/main/java/ru/naumkin/tm/entity/AbstractEntity.java
@@ -4,19 +4,26 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.jetbrains.annotations.NotNull;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
+@MappedSuperclass
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class AbstractEntity implements Serializable {
 
+    @Id
     @NotNull
+    @Column(name = "id")
     private String id = UUID.randomUUID().toString();
 
     @NonNull
+    @Column(name = "name")
     private String name;
 
 }
